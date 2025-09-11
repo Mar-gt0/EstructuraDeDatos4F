52775a48595e5ef010c96d153b63b60e694bc429
Mar-gt0 <marlent605@gmail.com>
2025-09-11 08:45:23 -0600

    cambien el nombre de mi clase

---
+++ EjercicioLista/src/SalariosPromedio.java
@@ -0,0 +1,6 @@
+public class SalariosPromedio {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Worldddd!");
+        System.out.println("Hello, Worldddd!");
+    }
+}
