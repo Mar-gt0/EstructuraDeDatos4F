4bded0b83952c140eb5c6aaaff744697b5e84867
20243ds165@utez.edu.mx <marlent605@gmail.com>
2025-09-11 20:14:00 -0600

    inicie en otro ejercicio de stack

---
+++ EjercicioLista/EjercicioStack/utez/edu/Historial.java
@@ -0,0 +1,24 @@
+package EjercicioLista.EjercicioStack.utez.edu;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Historial {
+    public static void main(String[] args) {
+        
+    Deque<String> pestañas = new ArrayDeque<>();
+    pestañas.push("sisa_utez");
+    pestañas.push("adidas_mexico");
+    pestañas.push("zara_mx");
+
+    for(String pestaña : pestañas){
+            System.out.println(pestaña);
+        }
+
+    System.out.println("Se quita del historial: "+ pestañas.pop());
+    System.out.println("En este momento te encuentras navegando en: ");
+
+
+
+    }
+}
