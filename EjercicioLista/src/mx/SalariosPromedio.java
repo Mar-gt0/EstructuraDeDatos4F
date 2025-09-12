c25e2732216449bc3303968a7b04741b366d56e5
20243ds165@utez.edu.mx <marlent605@gmail.com>
2025-09-11 18:51:32 -0600

    hice unos cuantos cambiois y arreh¿gke un error

---
+++ EjercicioLista/src/mx/SalariosPromedio.java
@@ -0,0 +1,9 @@
+package mx;
+
+public class SalariosPromedio {
+    public static void main(String[] args) throws Exception {
+       Trabajador trabajador = new Trabajador();
+       trabajador.getNombre("pedro");
+        
+    }
+}
