973f8b8f49ca6148a495b23664fc36a66f3f2ed0
20243ds165@utez.edu.mx <marlent605@gmail.com>
2025-09-11 20:26:17 -0600

    quite codigo innecesario que no use

--- EjercicioLista/src/mx/Trabajador.java
+++ EjercicioLista/src/mx/Trabajador.java
@@ -9,39 +9,13 @@ public double salario;
 public Trabajador (){
 
 }
-
 public Trabajador (int id, String nombre, double salario){
 this.id = id;
 this.nombre = nombre;
+
 this.salario = salario;
 }
 
-public String getNombre(){
-    return nombre;
-}
-
-public void setNombre( String nombre){
-    this.nombre = nombre;
-}
-
-
-public int getId(){
-    return id;
-}
-
-public void setId( int id){
-    this.id = id;
-}
-
-
-
-public double getSalario(){
-    return salario;
-}
-
-public void setSalario( double salario){
-    this.salario = salario;
-}
 
  public String toString() {
         return "ID: " + id + ", Nombre: " + nombre + ", Salario: $" + salario;
