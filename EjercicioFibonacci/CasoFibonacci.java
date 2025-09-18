fc9082303c1cdea45d4c1eee94e57ea05737258f
Mar-gt0 <marlent605@gmail.com>
2025-09-18 08:53:06 -0600

    hice el nuevo ejercicion de fibonacci

---
+++ EjercicioFibonacci/CasoFibonacci.java
@@ -0,0 +1,22 @@
+package EjercicioFibonacci;
+
+public class CasoFibonacci {
+
+    public static int fibonacci(int n) {
+            if (n <= 1) {
+                return n;
+            }else{
+                return fibonacci(n - 1) +  fibonacci(n - 2);
+                
+            }
+        }
+    public static void main(String[] args) {
+      int resultado = fibonacci(5);
+    
+        for (int i = 0; i < resultado; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    }
+
