579c93af964dafccb3f7e0dbd9453f100129aab0
Mar-gt0 <marlent605@gmail.com>
2025-09-19 07:43:19 -0600

    termine el

--- EjercicioSuma/SumaRecursiva.java
+++ EjercicioSuma/SumaRecursiva.java
@@ -2,4 +2,22 @@ package EjercicioSuma;
 
 public class SumaRecursiva {
 
+    public static int SumaRecur(int numero){
+        if(numero <= 0){
+            return numero;
+        }else{
+            return numero + SumaRecur(numero - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int resultado = SumaRecur(3);
+
+            System.out.println(resultado);
+        
+
+
+    }
+
 }
+
