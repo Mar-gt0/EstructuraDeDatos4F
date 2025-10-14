53f677d3dbbac4853daddfea8dc99594b1446ab8
Mar-gt0 <marlent605@gmail.com>
2025-10-13 19:15:16 -0600

    Termine el ejercicio de marcadores (tarea)

--- InsertarMarcadores/Marcadores.java
+++ InsertarMarcadores/Marcadores.java
@@ -3,9 +3,8 @@ package InsertarMarcadores;
 import java.util.Arrays;
 
 public class Marcadores {
-        public static void main(String[] args) {
-            int nuevoValor;
-             int[] filaOriginal;
+    public static void main(String[] args){
+            int nuevoNumero;
             int [][] arregloOriginal = {
             {1},           // impar -> agrega 0
             {6,3,1},       // impar -> agrega 0
@@ -15,36 +14,43 @@ public class Marcadores {
             {2,4,8,9,10}   // impar -> agrega 0
             };
 
-
             System.out.println("--- Arreglo Original ---");
                 for(int i = 0; i < arregloOriginal.length; i++){
                     for(int j = 0; j < arregloOriginal[i].length; j++){
                         System.out.print(arregloOriginal[i][j]);
+                        System.out.print(" ");
                     }
                     System.out.println();
                 }
 
-                int [][] arregloModificado = new int [arregloOriginal.length][];
-
+                 int[][] arregloModificado = new int[arregloOriginal.length][];
                  for (int i = 0; i < arregloOriginal.length; i++) {
-                    filaOriginal = arregloOriginal[i];
-                    if(filaOriginal.length % 2 != 0){
-                     nuevoValor = filaOriginal[0] + filaOriginal[filaOriginal.length - 1];
-                 }else{
-                     nuevoValor = 0;
+                      int[] filaOriginal = arregloOriginal[i];
+                        if(filaOriginal.length % 2 != 0){
+                             nuevoNumero = 0;
+                        }else{
+                             nuevoNumero = (filaOriginal[filaOriginal.length-1]) + (filaOriginal[0]);
+                        }
+
+                        int[] filaNueva = new int[filaOriginal.length + 1];
+
+                        for (int j = 0; j < filaOriginal.length; j++) {
+                            filaNueva[j] = filaOriginal[j];
+                         }
+                        filaNueva[filaNueva.length - 1] = nuevoNumero;
+
+                         arregloModificado[i] = filaNueva;
                  }
 
-                 int[] filaNueva = new int[filaOriginal.length+1];
-                 
-                  for (int j = 0; j < filaNueva.length; j++) {
-                    filaNueva[j] = filaOriginal[j];
-                  }
-                
-                
-                 System.out.println("--- Arreglo modificado ---");
-                    for (int[] fila : arregloModificado) {
-                        System.out.println(Arrays.toString(fila));
-                    }}
+                  System.out.println("--- Arreglo modificado ---");
+                    for (int i = 0; i < arregloModificado.length; i++) {
+                        for (int j = 0; j < arregloModificado[i].length; j++) {
+                            System.out.print(arregloModificado[i][j]);
+                            System.out.print(" ");
+                        }
+            
+                        System.out.println();
+    }
 
     }// final del main
 }// final de la clase
